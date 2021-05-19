9003688baeab5121a1e37472f770561e2da2b939
melaboudi <melaboudi@gmail.com>
2021-05-19 08:30:41 +0100

    initially working

--- src/main.cpp
+++ src/main.cpp
@@ -4,20 +4,30 @@ bool received =false;
 
 void setup() {
   Wire.begin();
-  Serial.begin(9600);
+  Serial.begin(115200);
+  pinMode(2,OUTPUT);
 
+  // Wire.beginTransmission(8); // transmit to device #8
+  // Wire.write("0,0,0");        // sends five bytes
+  // Wire.endTransmission();
 }
 
 void loop() {
-  delay(300);
-  Wire.requestFrom(8, 4);
-  byte lb1; byte hb1; byte lb2; byte hb2;
-  while (Wire.available()){lb1=Wire.read();hb1=Wire.read();lb2=Wire.read();hb2=Wire.read();received=true;}
-    if(received){
-      char str1[3];sprintf(str1, "%d", word(hb1,lb1));
-      char str2[3];sprintf(str2, "%d", word(hb2,lb2));
-      Serial.print("str1 = ");Serial.println(str1);
-      Serial.print("str2 = ");Serial.println(str2);
-      received=false;
-    }else {Serial.println("Nothing received");}
+  Wire.beginTransmission(8); // transmit to device #8
+  Wire.write("1,5000,800");        // start,scanning time,power
+  Wire.endTransmission();
+  delay(10000);
+
+  for (uint8_t i = 0; i < 10; i++)
+  {
+    Wire.requestFrom(8, 24);    // request 6 bytes from slave device #8
+    Serial.print("tag # ");Serial.print(i);Serial.print("= ");
+    while (Wire.available()) { // slave may send less than requested
+    char c = Wire.read(); // receive a byte as character
+    Serial.print(c);         // print the character
+    }Serial.println("");
+  }
+
+
+
 }
